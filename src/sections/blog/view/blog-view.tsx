dfdb4d29c86d726e378e398fb6795ad9ee12a371
Vikrant Pise <vikrantpise98@gmail.com>
2025-01-25 16:17:28 +0530

    grid erros solve

--- src/sections/blog/view/blog-view.tsx
+++ src/sections/blog/view/blog-view.tsx
@@ -3,6 +3,8 @@ import { useState, useCallback } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 // import Grid from '@mui/material/Unstable_Grid2';
+import Grid from '@mui/material/Grid';
+
 import Typography from '@mui/material/Typography';
 import Pagination from '@mui/material/Pagination';
 
